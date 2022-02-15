9a6bb64cbb7239878946b68caed4b52b6ac7f72d
jeong-inn <jung1nn@naver.com>
2022-02-15 13:54:23 +0900

    fix : comment

--- src/27_dc_motor/01._dc_motor_sample1.py
+++ src/27_dc_motor/01._dc_motor_sample1.py
@@ -1,24 +1,43 @@
+# ******************************************************************************************
+# FileName     : 01._dc_motor_sample1
+# Description  : DC모터를 이용한 바퀴 전진 해보기
+# Author       : 손정인
+# Created Date : 2022.02.15
+# Reference    :
+# Modified     : 
+# ******************************************************************************************
+
+# import
 import time
 from machine import Pin
 from ETboard.lib.pin_define import *
 
-led_red = Pin(D2)
-led_blue = Pin(D3)
+# global variable
+led_red = Pin(D2)                              # 빨강 LED 핀 지정
+led_blue = Pin(D3)                             # 파랑 LED 핀 지정
 
+# setup
 def setup() :
-    led_red.init(Pin.OUT)
-    led_blue.init(Pin.OUT)
+    led_red.init(Pin.OUT)                      # 빨강 LED 출력모드 설정
+    led_blue.init(Pin.OUT)                     # 파랑 LED 출력모드 설정
     
+# main loop
 def loop() :
-    led_red.value(HIGH)
-    led_blue.value(HIGH)
-    time.sleep(5)
+    led_red.value(HIGH)                        # 빨강 LED 켜기
+    led_blue.value(LOW)                        # 파랑 LED 끄기
+    time.sleep(5)                              # 5초 기다리기
     
-    led_red.value(LOW)
-    led_blue.value(LOW)
-    time.sleep(5)
+    led_red.value(LOW)                         # 빨강 LED 끄기
+    led_blue.value(LOW)                        # 파랑 LED 끄기
+    time.sleep(5)                              # 5초 기다리기
 
 if __name__ == "__main__" :
     setup()
     while True :
         loop()
+        
+# ==========================================================================================
+#
+#  (주)한국공학기술연구원 http://et.ketri.re.kr
+#
+# ==========================================================================================
