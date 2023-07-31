a7c16c97d6d4a19fd0c59028b5f2cc2b9054617d
kpej <126046230+kpej@users.noreply.github.com>
2023-07-31 18:05:20 +0900

    Edit Servo pin

--- src/21_servo_motor/03._servo_motor_up_down/03._servo_motor_up_down.py
+++ src/21_servo_motor/03._servo_motor_up_down/03._servo_motor_up_down.py
@@ -1,6 +1,6 @@
 # ******************************************************************************************
 # FileName     : 03._servo_motor_up_down
-# Description  : 빨강, 노랑 버튼을 눌러 서보모터 움직여 보기
+# Description  : 파랑, 초록 버튼을 눌러 서보모터 움직여 보기
 # Author       : 이승찬
 # Created Date : 2021.08.20
 # Reference    :
@@ -16,27 +16,27 @@ from ETboard.lib.servo import Servo
 
 
 # global variable
-servo = Servo(Pin(D2))                         # 서보모터 핀 지정
-Up = Pin(D6)                                   # 빨강 버튼 핀 지정
-Down = Pin(D9)                                 # 노랑 버튼 핀 지정
+servo = Servo(Pin(D6))                         # 서보모터 핀 지정
+Up = Pin(D7)                                   # 파랑 버튼 핀 지정
+Down = Pin(D8)                                 # 초록 버튼 핀 지정
 
 
 # setup
 def setup():
-    Up.init(Pin.IN)                            # 빨강 버튼 입력모드 설정
-    Down.init(Pin.IN)                          # 노랑 버튼 입력모드 설정
+    Up.init(Pin.IN)                            # 파랑 버튼 입력모드 설정
+    Down.init(Pin.IN)                          # 초록 버튼 입력모드 설정
 
 
 # mainloop
 def loop():
-    Up_state = Up.value()                      # 빨강 버튼값 가져오기
-    Down_state = Down.value()                  # 노랑 버튼값 가져오기
+    Up_state = Up.value()                      # 파랑 버튼값 가져오기
+    Down_state = Down.value()                  # 초록 버튼값 가져오기
     
-    if Up_state == LOW:                        # 뻘강 버튼이 눌리면 서보모터 180도 까지 회전
+    if Up_state == LOW:                        # 파랑 버튼이 눌리면 서보모터 180도 까지 회전
         servo.write_angle(180)
         time.sleep(0.3)
                 
-    if Down_state == LOW:                      # 노랑 버튼이 눌리면 서보모터 0도 까지 회전
+    if Down_state == LOW:                      # 초록 버튼이 눌리면 서보모터 0도 까지 회전
         servo.write_angle(0)
         time.sleep(0.3)
 
--- src/21_servo_motor/04._servo_motor_control/04._servo_motor_control.py
+++ src/21_servo_motor/04._servo_motor_control/04._servo_motor_control.py
@@ -1,6 +1,6 @@
 # ******************************************************************************************
 # FileName     : 04._servo_motor_control
-# Description  : 발강, 노랑 버튼을 눌러 서보모터를 제어 해보기
+# Description  : 파랑, 초록 버튼을 눌러 서보모터를 제어 해보기
 # Author       : 이승찬
 # Created Date : 2021.08.20
 # Reference    :
@@ -16,33 +16,33 @@ from ETboard.lib.servo import Servo
 
 
 # global variable
-servo = Servo(Pin(D2))                         # 서보모터 핀 지정
-Up = Pin(D6)                                   # 빨강 버튼 핀 지정
-Down = Pin(D9)                                 # 노랑 버튼 핀 지정
+servo = Servo(Pin(D6))                         # 서보모터 핀 지정
+Up = Pin(D7)                                   # 파랑 버튼 핀 지정
+Down = Pin(D8)                                 # 초록 버튼 핀 지정
 pos = 0
 
 
 # setup
 def setup():
-    Up.init(Pin.IN)                            # 빨강 버튼 입력모드 설정
-    Down.init(Pin.IN)                          # 노랑 버튼 입력모드 설정
+    Up.init(Pin.IN)                            # 파랑 버튼 입력모드 설정
+    Down.init(Pin.IN)                          # 초록 버튼 입력모드 설정
     
 
 # mainloop
 def loop():
     global pos
     
-    Up_state = Up.value()                      # 빨강 버튼값 가져오기
-    Down_state = Down.value()                  # 노랑 버튼값 가져오기
+    Up_state = Up.value()                      # 파랑 버튼값 가져오기
+    Down_state = Down.value()                  # 초록 버튼값 가져오기
     
-    if Up_state == LOW:                        # 빨강 버튼이 눌리면 서보모터의 각도 1도씩 증가
+    if Up_state == LOW:                        # 파랑 버튼이 눌리면 서보모터의 각도 1도씩 증가
         pos += 1
         if pos > 180:
             pos = 180                          # 서보모터의 각도가 180도 이상이 되지 않게 설정
         servo.write_angle(pos)
         time.sleep(0.01)
     
-    if Down_state == LOW:                      # 노랑 버튼이 눌리면 서보모터의 각도 1도씩 감소
+    if Down_state == LOW:                      # 초록 버튼이 눌리면 서보모터의 각도 1도씩 감소
         pos -= 1
         if pos < 0:
             pos = 0                            # 서보모터의 각도가 0도 이하가 되지 않게 설정
