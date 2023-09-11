05532ba8bee90172a00e3ae741973cea1f848e92
ktw2873 <ktw2873@naver.com>
2023-09-11 13:08:31 +0900

    edit code

--- src/28_dht/01._dht11/01._dht11.py
+++ src/28_dht/01._dht11/01._dht11.py
@@ -4,7 +4,7 @@
 # Author       : 손철수
 # Created Date : 2023.07.28
 # Reference    :
-# Modified     : 
+# Modified     : 2023.08.17 : KTW : 코드 수정
 # ******************************************************************************************
 
 
@@ -14,21 +14,23 @@ from machine import Pin
 from ETboard.lib.pin_define import *
 import dht
 
+
 # global variable
-sensor = dht.DHT11(Pin(D2))           # 온습도(DHT11) 센서 핀 지정
+sensor = dht.DHT11(Pin(D2))                      # 온습도(DHT11) 센서 핀 지정
+
 
 # setup
 def setup():
-    pass                              # 아무것도 안함
+    pass                                         # 아무것도 안함
 
 
 # main loop
 def loop():
-    sensor.measure()                  # 온습도 센서 값 측정
-    print(sensor.temperature(),       # 온도 값 출력
-          sensor.humidity())          # 습도 값 출력
-    
-    time.sleep(1)                     # 1초 기다리기
+    sensor.measure()                             # 온습도 센서 값 측정
+    print(str(sensor.temperature()) + 'C',       # 온도 값 출력
+          str(sensor.humidity()) + '%')          # 습도 값 출력
+
+    time.sleep(1)                                # 1초 기다리기
 
 
 if __name__ == "__main__":
--- src/51_ultrasonic_servo_motor/ultrasonic_servo_motor/ultrasonic_servo_motor.py
+++ src/51_ultrasonic_servo_motor/ultrasonic_servo_motor/ultrasonic_servo_motor.py
@@ -4,9 +4,9 @@
 # Author       : 이승찬
 # Created Date : 2021.08.20
 # Reference    :
-# Modified     : 2022.02.08 : SJI : 헤더 수정, 주석 수정 
+# Modified     : 2022.02.08 : SJI : 헤더 수정, 주석 수정
+# Modified     : 2023.09.11 : KTW : 코드 수정 
 # ******************************************************************************************
-
 
 # import
 import time
@@ -16,7 +16,7 @@ from ETboard.lib.servo import Servo
 
 
 # global variable
-servo = Servo(Pin(D2))                           # 서보모터 핀 지정
+servo = Servo(Pin(D6))                           # 서보모터 핀 지정
 trigPin = Pin(D9)                                # 초음파 송신부 핀 지정
 echoPin = Pin(D8)                                # 초음파 수신부 핀 지정
 
@@ -25,7 +25,7 @@ echoPin = Pin(D8)                                # 초음파 수신부 핀 지�
 def setup():
     trigPin.init(Pin.OUT)                        # 초음파 송신부 출력모드 설정
     echoPin.init(Pin.IN)                         # 초음파 수신부 입력모드 설정
-    
+
 
 # main loop
 def loop():
@@ -38,13 +38,16 @@ def loop():
 
     duration = time_pulse_us(echoPin, HIGH)
     distance = 17 * duration / 1000
-    
-    if distance < 20:                           # 물체와의 거리가 20cm 미만이면 180도로 설정
+
+    if distance < 20:                            # 물체와의 거리가 20cm 미만이면 180도로 설정
         servo.write_angle(180)
-    
-    if distance >= 20:                          # 물체와의 거리가 20cm 이상이면 0도로 설정
+
+    if distance >= 20:                           # 물체와의 거리가 20cm 이상이면 0도로 설정
         servo.write_angle(0)
-        
+
+    print(f'{distance : .2f}', "cm")             # 거리를 화면에 출력해줌
+    time.sleep(0.5)                              # 0.5초 대기
+
 
 if __name__ == "__main__":
     setup()
